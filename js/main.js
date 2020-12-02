1af53c347cc4dce54e093b3b58189b4f817a376a
ricardocosta78 <ricardo.costa78@gmail.com>
2020-12-02 09:06:55 +0000

    final biografia.html

--- js/main.js
+++ js/main.js
@@ -12,3 +12,43 @@ close.addEventListener('click',function(){
     header.classList.remove('menu-open')
 })
 
+function mostrarCard1(){
+    document.getElementById('card1').style.display='flex'
+    document.getElementById('card1').style.alignItems="center"
+    document.getElementById('card1').style.justifyContent = "center"
+
+    document.getElementById('card2').style.display='none'
+    document.getElementById('card3').style.display='none'
+    document.getElementById('card4').style.display='none'
+    document.getElementById('main').style.display='none'
+}
+
+function mostrarCard2(){
+    document.getElementById('card2').style.display='flex'
+    document.getElementById('card2').style.alignItems="center"
+    document.getElementById('card2').style.justifyContent = "center"
+    document.getElementById('card3').style.display='none'
+    document.getElementById('card4').style.display='none'
+    document.getElementById('card1').style.display='none'
+    document.getElementById('main').style.display='none'
+}
+function mostrarCard3(){
+    document.getElementById('card3').style.display='flex'
+    document.getElementById('card3').style.alignItems="center"
+    document.getElementById('card3').style.justifyContent = "center"
+    document.getElementById('card2').style.display='none'
+    document.getElementById('card4').style.display='none'
+    document.getElementById('card1').style.display='none'
+    document.getElementById('main').style.display='none'
+}
+function mostrarCard4(){
+    document.getElementById('card4').style.display='inline'
+    document.getElementById('card4').style.display='flex'
+    document.getElementById('card4').style.alignItems="center"
+    document.getElementById('card4').style.justifyContent = "center"
+    document.getElementById('card3').style.display='none'
+    document.getElementById('card2').style.display='none'
+    document.getElementById('card1').style.display='none'
+    document.getElementById('main').style.display='none'
+}
+
