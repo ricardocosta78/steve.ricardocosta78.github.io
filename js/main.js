06f3748e9e478b75311d176d512efa557c0393d3
ricardocosta78 <ricardo.costa78@gmail.com>
2020-12-04 08:14:01 +0000

    PROJECTO PRONTO

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 let header = document.querySelector('.header');
 let hamburguerMenu = document.querySelector('.hamburguer-menu');
-let close = document.querySelector('.close');
+let close = document.querySelector('.hamburguer-menu-close');
 
 hamburguerMenu.addEventListener('click',function(){
     header.classList.add('menu-open');
